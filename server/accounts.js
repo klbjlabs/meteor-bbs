b5cfd8674cb4717b8c7207eb6ab7537623abbbfa
limingth <limingth@gmail.com>
2015-02-05 02:23:28 -0800

    Github login with gravatar

--- server/accounts.js
+++ server/accounts.js
@@ -11,7 +11,25 @@ Accounts.onCreateUser(function(options,user){
     user.profile.github.accessToken = user.services.github.accessToken;
     user.profile.github.email = user.services.github.email;
     user.profile.github.username = user.services.github.username;
-    user.profile.github.avatar_url = user.services.github.avatar_url;
+
+    var email = user.services.github.email;
+    var options = { 
+      secure: true // choose between `http://www.gravatar.com` 
+                 //            and `https://secure.gravatar.com`
+                 //            default is `false`
+    }; 
+
+				var md5Hash = Gravatar.hash(email);
+				// 5658ffccee7f0ebfda2b226238b1eb6e
+
+				var url = Gravatar.imageUrl(email, options);
+				// https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e
+
+				var url2 = Gravatar.imageUrl(md5Hash, options);
+				// https://secure.gravatar.com/avatar/5658ffccee7f0ebfda2b226238b1eb6e
+
+    user.profile.github.avatar_url = url2; 
+    user.profile.figureUrlAt50 = url2;
 
     return user;
 });
